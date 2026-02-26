e4847b6fd5ec38e551a5170c3680e0ccc7a876e3
Mike polikarpov <polikarpov@cae-fidesys.com>
2026-02-26 13:46:36 +0300

    use ollama

--- create_vector_db.py
+++ create_vector_db.py
@@ -5,7 +5,7 @@ from langchain.schema import Document
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain_community.embeddings import HuggingFaceEmbeddings
-
+'''
 PROJECT_DIR = os.path.dirname(__file__)
 HELP_PATH = os.path.join(PROJECT_DIR,'help') # замените на ваш путь
 
@@ -22,6 +22,7 @@ def add_html_paths(folder_path) -> List[str]:
 
 # Загружаем HTML файлы
 html_paths = add_html_paths(HELP_PATH)
+
 
 # Класс для извлечения текста из HTML
 class MyBSHTMLLoader:
@@ -61,3 +62,25 @@ if __name__ == '__main__':
     faiss_db.save_local("my_faiss_index")
 
     print('База успешно сохранена')
+'''
+from langchain_community.document_loaders import TextLoader
+from langchain_qdrant import Qdrant
+from langchain_text_splitters import CharacterTextSplitter
+from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import RunnablePassthrough
+from langchain_core.output_parsers import StrOutputParser
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from qdrant_client import QdrantClient
+from langchain_qdrant import QdrantVectorStore
+
+loader = TextLoader("file.txt",encoding = 'utf-8')
+documents = loader.load()
+text_splitter = CharacterTextSplitter(chunk_size=508,chunk_overlap=20)
+docs = text_splitter.split_documents(documents)
+embeddings = HuggingFaceEmbeddings(model_name="intfloat/multilingual-e5-small")
+
+vectorestore = QdrantVectorStore.from_documents(
+    docs,
+    embeddings,
+    path="./qdrant_data",
+    collection_name="my_collection")
--- do_prompts.py
+++ do_prompts.py
@@ -2,8 +2,14 @@ from langchain_community.vectorstores import FAISS
 from sentence_transformers import SentenceTransformer
 import faiss
 import os
-from create_vector_db import texts
+from langchain_core.prompts import PromptTemplate
+from create_vector_db import vectorestore
+from langchain_community.chat_models import ChatOllama
+from langchain_core.runnables import RunnablePassthrough
+from langchain_core.output_parsers import StrOutputParser
+from transformers import AutoModelForCausalLM
 
+'''
 DIR_NAME = os.path.dirname(__file__)
 index = faiss.read_index(os.path.join(DIR_NAME, 'my_faiss_index', 'index.faiss'))
 model = SentenceTransformer('paraphrase-multilingual-MiniLM-L12-v2')
@@ -11,10 +17,40 @@ query_text = 'APREPRO'
 query_vector = model.encode(query_text, convert_to_numpy=True).astype('float32')
 distances, indices = index.search(query_vector.reshape(1, -1), k=20)
 '''
+'''
 for index in indices[0]:
     print(texts[index])
     
     print('______________________________________')
 '''
+'''
+
 new_texts = list(map(lambda el: texts[el], indices[0]))
 print(new_texts)
+'''
+retriever = vectorestore.as_retriever()
+template = """
+Используй контекст, чтобы ответить на вопрос. 
+Можешь использовать свои знания на русском языке
+Отвечай только по сути, без вступительных пояснений и английских фраз.
+Если не знаешь что ответить, пиши - Я не знаю
+Дай ** 3 разных варианта ответа** в виде списка на языке програмирования python
+Контекст:
+{context}
+
+Вопрос:
+{question}
+
+Ответ:
+"""
+
+custom_rag_prompt = PromptTemplate.from_template(template)
+llm = ChatOllama(model="llama3")
+#llm = AutoModelForCausalLM.from_pretrained("NousResearch/Nous-Hermes-13b", device_map="auto")
+rag_chain = (
+        {"context": retriever, "question": RunnablePassthrough()}
+        | custom_rag_prompt
+        | llm
+        | StrOutputParser()
+)
+print(rag_chain.invoke("Какой алгоритм работы KNN"))
