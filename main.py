1b05a0c0ddf90833f178d2b177f1ec85263b0e07
Михаил Поликарпов <mihailpolikarpov@Mac.Dlink>
2026-05-04 14:52:16 +0300

    add readme-file in parsing logs project

--- main.py
+++ main.py
@@ -1,48 +1,91 @@
 import asyncio
 import streamlit as st
 import os
+from langchain_openai import ChatOpenAI
 from langchain_qdrant import QdrantVectorStore
-from langchain_community.embeddings import HuggingFaceEmbeddings
-from gigachat import GigaChat
+from config import settings
+from langchain.prompts import PromptTemplate
+from langchain.chains import RetrievalQA
+from langchain_openai import OpenAIEmbeddings
+import streamlit as st
 
-key = 'YOUR_KEY_HERE'
-st.title('My first App')
-
-embed_model = HuggingFaceEmbeddings(
-    model_name="paraphrase-multilingual-MiniLM-L12-v2"
+LLM = ChatOpenAI(
+    api_key=settings.OPEN_AI_KEY.get_secret_value(),
+    base_url=settings.BASE_URL,
+    model='gpt-5.4-mini',
+    timeout=30
 )
 
-vectorestore = QdrantVectorStore.from_existing_collection(
+# embs = HuggingFaceEmbeddings(model_name='intfloat/multilingual-e5-large')
+
+embs = OpenAIEmbeddings(
+    api_key=settings.OPEN_AI_KEY.get_secret_value(),
+    base_url=settings.BASE_URL,
+    model="text-embedding-3-large"
+)
+
+vectorstore = QdrantVectorStore.from_existing_collection(
     path="./qdrant_data",
     collection_name="my_collection",
-    embedding=embed_model
+    embedding=embs,
 )
-retriever = vectorestore.as_retriever(search_kwargs={"k": 6})
 
-async def get_promt(query_text):
-    results = retriever.get_relevant_documents(query_text)
-    async with GigaChat(credentials=key, verify_ssl_certs=False) as giga:
-        context = "\n".join([doc.page_content for doc in results])
-        prompt = f"""
-Ты эксперт по документации Fidesys.
-Отвечай на основе переданного контекста.
-Не используй внешние знания.
-Если информации нет — отвечай: В документации нет информации.
+retriever = vectorstore.as_retriever(
+    search_type="mmr",
+    search_kwargs={"k": 8,
+                   "fetch_k": 40,
+                   "lambda_mult": 0.5}
+)
+
+template = """
+Ты отвечаешь строго по документации.
+Ответ должен быть в обычном тексте без Markdown-разметки
+Правила:
+1. Используй только информацию из документов
+2. Разрешено переформулировать и обобщать
+3. Если есть несколько фрагментов:
+   - выбери тот, который наиболее полно отвечает на вопрос
+   - приоритет имеют перечисления и явные исключения
+4. Формулировки вида:
+   - "исключение составляют"
+   - "не включается"
+   - "не записывается"
+   считать прямым ответом на вопрос
+5. Используй ВСЮ релевантную информацию, а не первое совпадение
+6. Если релевантной информации нет → напиши:
+"Ответ не найден в документации"
+
+Дай развернутый ответ.
+
 Документы:
 {context}
 
-Вопрос: {query_text}
+Вопрос:
+{question}
 
 Ответ:
 """
-        response = await giga.chat(prompt)
-        return response.choices[0].message.content
+
+prompt = PromptTemplate(
+    template=template,
+    input_variables=["context", "question"]
+)
 
 
-with st.form(key='my_form'):
-    question = st.text_input("Введите вопрос")
-    submitted = st.form_submit_button("Спросить")
-    if submitted:
-        # запускаем async функцию через asyncio.run()
-        answer = asyncio.run(get_promt(question))
-        st.write(answer)
+
+with st.form(key = "search_form") :
+    query_col,button_col = st.columns([4,1])
+    query = query_col.text_input("",placeholder= "input query")
+    docs = retriever.get_relevant_documents(query)
+    sub_button = button_col.form_submit_button("click",use_container_width=True)
+    qa_chain = RetrievalQA.from_chain_type(
+        llm=LLM,
+        retriever=retriever,
+        # chain_type="refine",
+        chain_type_kwargs={"prompt": prompt},
+        return_source_documents=True
+    )
+    response = qa_chain.invoke({"query": query})
+
+if query and sub_button:
+    st.write(response['result'])
